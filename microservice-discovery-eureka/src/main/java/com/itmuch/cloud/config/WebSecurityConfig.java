3dde3ac3fcaec5486ebe9ac7348e07e722cf0862
wangwyx <wangongxinwyx@163.com>
2019-08-19 11:05:24 +0800

    更新版本后的eureka权限更改

---
+++ microservice-discovery-eureka/src/main/java/com/itmuch/cloud/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.itmuch.cloud.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @auth: wangyx-p
+ * @date :2019/8/19 10:50
+ * @Description:
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
